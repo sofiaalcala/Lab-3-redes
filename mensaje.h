a52a73e7296c92af874d9514dc7c77f45b7910a5
sofiaalcala <157811713+sofiaalcala@users.noreply.github.com>
2026-03-23 00:30:38 -0500

    Update mensaje.h

--- mensaje.h
+++ mensaje.h
@@ -1,6 +1,11 @@
+#ifndef MENSAJE_H
+#define MENSAJE_H
+
+#include <stdint.h>
 typedef struct {
     char tipo[16];        // "PUBLISH", "SUBSCRIBE", "ACK"
     char tema[32];        // "PartidoA", "PartidoB"
     char contenido[256];  // "Gol de Equipo A al minuto 32"
     uint32_t seq_num;     // numero de secuencia
 } Mensaje;
+#endif

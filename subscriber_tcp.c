f43decd1c7bbe6be04413340f522497e176b23bc
sofiaalcala <157811713+sofiaalcala@users.noreply.github.com>
2026-03-23 00:32:14 -0500

    Create subscriber_tcp.c

---
+++ subscriber_tcp.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include "mensaje.h"
+
+#define PORT 8080
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        printf("Uso: %s <tema>\n", argv[0]);
+        printf("Temas validos: PartidoA o PartidoB\n");
+        return 1;
+    }
+
+    char *tema = argv[1];
+
+    if (strcmp(tema, "PartidoA") != 0 && strcmp(tema, "PartidoB") != 0) {
+        printf("Tema invalido. Use PartidoA o PartidoB\n");
+        return 1;
+    }
+
+    int sockfd;
+    struct sockaddr_in broker;
+    Mensaje msg;
+
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) {
+        perror("Error al crear socket TCP");
+        return 1;
+    }
+
+    memset(&broker, 0, sizeof(broker));
+    broker.sin_family = AF_INET;
+    broker.sin_port = htons(PORT);
+    broker.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+    if (connect(sockfd, (struct sockaddr *)&broker, sizeof(broker)) < 0) {
+        perror("Error en connect");
+        close(sockfd);
+        return 1;
+    }
+
+    memset(&msg, 0, sizeof(Mensaje));
+    strcpy(msg.tipo, "SUBSCRIBE");
+    strcpy(msg.tema, tema);
+    strcpy(msg.contenido, "Solicitud de suscripcion");
+    msg.seq_num = 0;
+
+    send(sockfd, &msg, sizeof(Mensaje), 0);
+
+    printf("Suscrito al tema %s en broker TCP puerto %d\n", tema, PORT);
+
+    while (1) {
+        int n = recv(sockfd, &msg, sizeof(Mensaje), 0);
+        if (n > 0) {
+            printf("Recibido -> tipo=%s tema=%s seq=%u contenido=%s\n",
+                   msg.tipo, msg.tema, msg.seq_num, msg.contenido);
+        } else {
+            printf("Conexion cerrada por el broker\n");
+            break;
+        }
+    }
+
+    close(sockfd);
+    return 0;
+}
